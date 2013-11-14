c242732e6b513a90047ae77f84a7660f7f4368c4
davide <d@vide.bz>
2013-11-14 19:50:52 +0100

    TextArea

---
+++ src/bz/davide/dmweb/shared/view/TextAreaView.java
@@ -0,0 +1,58 @@
+/*
+DMWeb - Java web framework - http://www.davide.bz/dmweb
+
+Copyright (C) 2013 Davide Montesin <d@vide.bz> - Bolzano/Bozen - Italy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+package bz.davide.dmweb.shared.view;
+
+import com.google.gwt.dom.client.TextAreaElement;
+
+/**
+ * @author Davide Montesin <d@vide.bz>
+ */
+public class TextAreaView extends AbstractHtmlElementView implements DivViewChild
+{
+
+   public TextAreaView(int rows, int cols)
+   {
+      super("textarea");
+      this.setElementAttribute("rows", String.valueOf(rows));
+      this.setElementAttribute("cols", String.valueOf(cols));
+   }
+
+   protected TextAreaView(Void void1)
+   {
+      super(void1);
+   }
+
+   public void setText(String value)
+   {
+      if (AbstractHtmlElementView.clientSide)
+      {
+         TextAreaElement.as(this.getElement()).setValue(value);
+      }
+      else
+      {
+         this.setElementAttribute("value", value);
+      }
+   }
+
+   public String getValue()
+   {
+      return TextAreaElement.as(this.getElement()).getValue();
+   }
+}
